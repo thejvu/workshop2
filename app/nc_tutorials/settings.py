15765e94c6b3cbbebb926ffae0944e7bc3ed7297
Johnny <vujohnny4@gmail.com>
2022-09-29 16:39:05 -0700

    debug allow host

--- app/nc_tutorials/settings.py
+++ app/nc_tutorials/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-(j-#aqq@vs$3l#0(031z4_h(_*pa&ye#dt&x69_@(&a6(=+fpg
 DEBUG = True
 
 ALLOWED_HOSTS = [
-    'ec2-50-1git12-171-19.us-west-2.compute.amazonaws.com', '0.0.0.0', 'localhost', '127.0.0.1'
+    'ec2-50-112-171-19.us-west-2.compute.amazonaws.com', '0.0.0.0', 'localhost', '127.0.0.1'
 ]
 
 
